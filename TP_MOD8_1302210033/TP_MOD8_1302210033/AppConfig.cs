2aac958ca8fdf772eda6d1d44b2702921443096a
raliauliya <121374492+raliauliya@users.noreply.github.com>
2023-04-09 21:01:41 +0700

    menyelesaikan program dengan runtime configuration

---
+++ TP_MOD8_1302210033/TP_MOD8_1302210033/AppConfig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+public class AppConfig
+{
+    public CovidConfig covidconvig;
+    public const string fileLocation = "C:\\Users\\Asus\\Documents\\Lia\\Telkom\\SEMS 4\\KPL\\TP\\TPMOD8\\TPMOD8\\TP_MOD8_1302210033\\TP_MOD8_1302210033";
+    public const string filePath = fileLocation + "convid_config.json";
+
+    public AppConfig()
+    {
+        try
+        {
+            ReadConfigFile();
+        }
+        catch
+        {
+            SetDefault();
+            WriteNewConfigFile();
+        }
+    }
+
+    private CovidConfig ReadConfigFile()
+    {
+        string configJSONData = File.ReadAllText(filePath);
+        covidconvig = JsonSerializer.Deserialize<CovidConfig>(configJSONData);
+        return covidconvig;
+    }
+
+    private void SetDefault()
+    {
+        string pesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini ";
+        string pesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        covidconvig = new CovidConfig("celcius",14,pesanDitolak,pesanDiterima);
+    }
+
+    private void WriteNewConfigFile()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        string jsonstring = JsonSerializer.Serialize(covidconvig,options);
+        File.WriteAllText(filePath, jsonstring);
+    }
+}
--- TP_MOD8_1302210033/TP_MOD8_1302210033/Program.cs
+++ TP_MOD8_1302210033/TP_MOD8_1302210033/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AppConfig appConfig = new AppConfig();
+        appConfig.covidconvig.UbahSatuan();
+
+        Console.Write($"Berapa suhu badan Anda saat ini? Dalam nilai {appConfig.covidconvig.satuan_suhu} : ");
+        double suhu = double.Parse( Console.ReadLine() );
+
+        Console.Write("Berapa hari yang lalu (perkiraan) Anda terakhir memiliki gejala demam? ");
+        int hari = int.Parse( Console.ReadLine() );
+
+        try
+        {
+            if(hari < 0)
+            {
+                throw new Exception("Invalid");
+            }
+            if(appConfig.covidconvig.satuan_suhu == "celcius")
+            {
+                if(suhu >= 36.5 && suhu <= 37.5 && hari < appConfig.covidconvig.batas_hari_demam)
+                {
+                    Console.WriteLine(appConfig.covidconvig.pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(appConfig.covidconvig.pesan_ditolak);
+                }
+            }
+            else
+            {
+                if(suhu >= 97.7 && suhu <= 99.5 && hari < appConfig.covidconvig.batas_hari_demam)
+                {
+                    Console.WriteLine(appConfig.covidconvig.pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(appConfig.covidconvig.pesan_ditolak);
+                }
+            }
+            
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
